d52738c40f2cd1458d36b4454930e20da336f2a6
eli-10-dev <elijah.gaylan@gmail.com>
2025-12-11 19:35:16 +0800

    Update and rename dummy.txt to price_outliers.sql

---
+++ sql/price_outliers.sql
@@ -0,0 +1,13 @@
+SELECT
+	MIN(selling_price) AS min_selling_price,
+	PERCENTILE_CONT(0.25) WITHIN GROUP (ORDER BY selling_price) AS q1,
+	PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY selling_price) AS median,
+	PERCENTILE_CONT(0.75) WITHIN GROUP (ORDER BY selling_price) AS q3,
+	MAX(selling_price) AS max_selling_price,
+	(PERCENTILE_CONT(0.25) WITHIN GROUP (ORDER BY selling_price) - 1.5 
+	 * (PERCENTILE_CONT(0.75) WITHIN GROUP (ORDER BY selling_price) 
+	 - PERCENTILE_CONT(0.25) WITHIN GROUP (ORDER BY selling_price))) AS price_lower_outlier,
+	(PERCENTILE_CONT(0.75) WITHIN GROUP (ORDER BY selling_price) + 1.5
+	 * (PERCENTILE_CONT(0.75) WITHIN GROUP (ORDER BY selling_price) 
+	 - PERCENTILE_CONT(0.25) WITHIN GROUP (ORDER BY selling_price))) AS price_km_upper_outlier
+FROM car_data_raw;
